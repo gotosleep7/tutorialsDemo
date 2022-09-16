055bf61efe986ea8fd4f8019bd91c92d43a98306
gotosleep7 <gotosleep7@163.com>
2022-09-17 02:27:08 +0800

    login界面增加脚本

--- Basics_Graph/Assets/UI/Login/Login.cs
+++ Basics_Graph/Assets/UI/Login/Login.cs
@@ -1,35 +1,102 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class Login : MonoBehaviour
 {
 
-     
+
     [SerializeField]
-    TextMeshPro userId;
+    TMP_InputField userIdInputField;
+
     [SerializeField]
-    TextMeshPro password;
+    TMP_InputField passwordInputField;
+
     [SerializeField]
     ToggleGroup loginModel;
-    // Start is called before the first frame update
+
+    [SerializeField]
+    Toggle AutoLogin;
+
+    [SerializeField]
+    Button loginButton;
+
+    [SerializeField]
+    GameObject FailMessage;
+
+
+    Color LoginBtnEnableColor;
+    Color LoginBtnDisableColor;
+
+    string userId, passwword;
+
     void Start()
     {
-        
+        ColorUtility.TryParseHtmlString("#233D57", out LoginBtnEnableColor);
+        ColorUtility.TryParseHtmlString("#CAD1D7", out LoginBtnDisableColor);
+        SetLoginButtonState(false);
     }
+    private void OnEnable()
+    {
+        userIdInputField?.onValueChanged.AddListener(OnUserIdChange);
+        passwordInputField?.onValueChanged.AddListener(OnPwdChange);
+    }
+
+    private void OnDisable()
+    {
+        userIdInputField?.onValueChanged.RemoveListener(OnUserIdChange);
+        passwordInputField?.onValueChanged.RemoveListener(OnPwdChange);
+
+    }
+    private void OnUserIdChange(string arg0)
+    {
+        userId = arg0;
+        CanLogin();
+    }
+    private void OnPwdChange(string arg0)
+    {
+        passwword = arg0;
+        CanLogin();
+    }
+
+
+    public void CanLogin()
+    {
+        if (userId != null && userId != "" && passwword != null && passwword != "")
+        {
+            SetLoginButtonState(true);
+        }
+        else
+        {
+            SetLoginButtonState(false);
+        }
+
+    }
+
+    public void SetLoginButtonState(bool isEnable)
+    {
+
+        loginButton.enabled = isEnable;
+        loginButton.image.color = isEnable ? LoginBtnEnableColor : LoginBtnDisableColor;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
 
-    public void OnClick()
+    public void OnLogin()
     {
-        Debug.Log("sdfasd");
+        Debug.Log("userId:" + userIdInputField.text);
+        Debug.Log("password:" + passwordInputField.text);
+        Debug.Log("loginModel:" + loginModel.GetFirstActiveToggle().name);
+        Debug.Log("AutoLogin:" + AutoLogin.isOn);
+
     }
 }
